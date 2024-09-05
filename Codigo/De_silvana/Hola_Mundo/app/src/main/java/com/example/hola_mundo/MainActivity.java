ba456f134770b4838a6961c186193c3f532b4b89
DaniloGuerraM <165096653+DaniloGuerraM@users.noreply.github.com>
2024-09-05 19:59:14 -0300

    androy

--- Codigo/De_silvana/Hola_Mundo/app/src/main/java/com/example/hola_mundo/MainActivity.java
+++ Codigo/De_silvana/Hola_Mundo/app/src/main/java/com/example/hola_mundo/MainActivity.java
@@ -1,5 +1,46 @@
 package com.example.hola_mundo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonGuess;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Configurar los insets de la ventana
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Encontrar el botón por su ID
+        buttonGuess = findViewById(R.id.my_button);
+
+        // Configurar el OnClickListener para el botón
+        buttonGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostrar "Hola Mundo" en un Toast
+                Toast.makeText(MainActivity.this, "Hola Mundo", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+
+/*
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -22,3 +63,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+*/
