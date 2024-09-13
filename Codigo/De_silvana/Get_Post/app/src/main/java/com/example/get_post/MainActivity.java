3c618a6bac351fc8243a540fe5165e74574bb8c7
DaniloGuerraM <165096653+DaniloGuerraM@users.noreply.github.com>
2024-09-12 22:05:25 -0300

    ANDROID studio

--- Codigo/De_silvana/Get_Post/app/src/main/java/com/example/get_post/MainActivity.java
+++ Codigo/De_silvana/Get_Post/app/src/main/java/com/example/get_post/MainActivity.java
@@ -1,73 +1,166 @@
 package com.example.get_post;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     public TextView textView;
-    public Button button;
+    public Button buttonget;
+    public Button buttonput;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
         textView = findViewById(R.id.texto_1);
-        button = findViewById(R.id.boton_1);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonget = findViewById(R.id.boton_get);
+        buttonput = findViewById(R.id.button_put);
+
+        buttonget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chequear();
+                chequear("GET");
+            }
+        });
+        buttonput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chequear("PUT");
             }
         });
     }
-    // chequea si estas conectrado a internet
-    public void chequear(){
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo;
-        networkInfo = connMgr.getActiveNetworkInfo();
+
+    // Chequea si estás conectado a internet
+    public void chequear(String metodo) {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            // Operaciones http
-            get();
+            // Operaciones HTTP
+            if ("GET".equals(metodo))
+            {
+                get();
+            } else if ("PUT".equals(metodo))
+            {
+                put();
+            }
         } else {
             // Mostrar errores
-            textView.setText("no conectado");
+            textView.setText("No conectado");
         }
     }
-    public void get()
-    {
-        textView.setText("estas conectado");
+
+    public void get() {
+        textView.setText("Asiendo un GET");
+        String url = "https://api.weatherapi.com/v1/current.json?q=eduardo&lang=es&key=87637c428a6a496098d230942242604";
+        new GetAPI().execute(url);
+    }
+
+
+    private class GetAPI extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return doGetRequest(urls[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                textView.setText(result);
+            } else {
+                textView.setText("Error al realizar la solicitud");
+            }
+        }
+    }
+
+    private String doGetRequest(String urlstr) throws MalformedURLException {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
         try {
-            URL url = new URL("https://api.weatherapi.com/v1/current.json?q=eduardo&lang=es&key=87637c428a6a496098d230942242604");
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            Toast.makeText(MainActivity.this, "siiiiuuuuuuuuu", Toast.LENGTH_SHORT).show();
-        } catch (Exception exception){
-            Toast.makeText((Context) MainActivity.this, (CharSequence) exception, Toast.LENGTH_SHORT).show();
+            URL url = new URL(urlstr);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK)
+                throw new IOException("Error de respuesta: " + responseCode);
+
+            reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder result = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            return result.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
-}
+    public void put(){
+        textView.setText("Asiendo un PUT");
+        String url = "La url";
+        String jsonString = "{\"name\":\"value\"}";
+        new PutAPI().execute(url, jsonString);
+    }
+    private class PutAPI extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected String doInBackground(String... params){
+            try {
+                return HacerPut(params[0], params[1]);
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                textView.setText(result);
+            } else {
+                textView.setText("Error al realizar la solicitud");
+            }
+        }
+
+    }
---
+++ Codigo/De_silvana/Permiso/app/src/main/java/com/example/permiso/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.permiso;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.Manifest;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
+import android.util.Log;
+import android.content.Context;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    public Button camara;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+        camara = findViewById(R.id.button_camara);
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cam();
+            }
+        });
+    }
+    public void cam()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == MY_PERMISSIONS_REQUEST_CAMERA) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permiso concedido", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Toast.makeText(this, "Foto tomada", Toast.LENGTH_SHORT).show();
+        }
+    }}
