e4c49d2507ac06b67ec28725974aabdf0b82d548
DaniloGuerraM <165096653+DaniloGuerraM@users.noreply.github.com>
2024-09-26 20:01:05 -0300

    algo

--- Codigo/De_silvana/Pantallas/app/src/main/java/com/example/pantallas/MainActivity.java
+++ Codigo/De_silvana/Pantallas/app/src/main/java/com/example/pantallas/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.pantallas;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +17,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +27,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        if (isLoggedIn) {
+            PantallaDeAsistencia();
+        }else{
+
+            final EditText usernameEditText = findViewById(R.id.editTextEjemplo1);
+            Button loginButton = findViewById(R.id.button2);
+
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String username = usernameEditText.getText().toString();
+
+                    if (!username.isEmpty() ) {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username", username);
+                        editor.putBoolean("isLoggedIn", true);
+                        editor.apply();
+
+                        PantallaDeAsistencia();
+                    }
+                }
+            });
+        }
+
+    }
+    public void PantallaDeAsistencia(){
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 }
--- Codigo/De_silvana/Pantallas/app/src/main/java/com/example/pantallas/SecondActivity.java
+++ Codigo/De_silvana/Pantallas/app/src/main/java/com/example/pantallas/SecondActivity.java
@@ -1,6 +1,12 @@
 package com.example.pantallas;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +26,10 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+
     }
+
 }
